f5850af7514337438a1920463a69e341d09d7f2b
ZapDos7 <ioanna.zapalidi@gmail.com>
2020-06-07 23:37:45 +0300

    organization

---
+++ server/server.cpp
@@ -0,0 +1,11 @@
+/*	
+Ioanna Zapalidi, sdi1400044	
+System Programming Project #3, Spring 2020	
+ */
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
